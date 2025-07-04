0583abe5997225b350d9a517713142ff3bc42aa5
shareenku <shareenku15@gmail.com>
2025-07-05 01:02:00 +0700

    navbar

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 function showPrice(productName, price) {
     alert(`${productName} harganya adalah ${price}`);
   }
+
+  const toggle = document.getElementById("menu-toggle");
+  const menu = document.getElementById("nav-menu");
+
+  toggle.addEventListener("click", () => {
+    toggle.classList.toggle("active");
+    menu.classList.toggle("show");
+  });
   
